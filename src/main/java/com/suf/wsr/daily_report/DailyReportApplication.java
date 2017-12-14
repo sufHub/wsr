8fc7f62ae7704002ef8ca526f2efbafd75fc9503
ShaikUmmerFaruk_D <ShaikUmmerFaruk_D@CHNMCT133232L.ad.infosys.com>
2017-12-14 15:51:47 +0530

    Initial IMport

---
+++ src/main/java/com/suf/wsr/daily_report/DailyReportApplication.java
@@ -0,0 +1,12 @@
+package com.suf.wsr.daily_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyReportApplication.class, args);
+	}
+}
