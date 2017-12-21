db63a42da18860de806f34e78f84c6fca19feabf
shaikummerfaruk_D <shaikummerfaruk_D@CHNMCT377021D.ad.infosys.com>
2017-12-21 12:56:15 +0530

    Changes

--- src/main/java/com/suf/wsr/daily_report/controller/DailyReportController.java
+++ src/main/java/com/suf/wsr/daily_report/controller/DailyReportController.java
@@ -31,20 +31,17 @@ import com.atlassian.util.concurrent.Promise;
 import com.suf.wsr.daily_report.intf.DailyReportException;
 import com.suf.wsr.daily_report.intf.DailyReportIntf;
 
-
 @Controller
 public class DailyReportController {
 
 	@Autowired
 	private @Qualifier(value = "dailyReport") DailyReportIntf dailyReport;
 
-
-	@RequestMapping(value="/login")
+	@RequestMapping(value = "/login")
 	@ResponseBody
-	boolean displayDocs1(
-			@RequestParam(value = "username", required = true) String username,
-			@RequestParam(value = "password", required = true) String password,
-			HttpServletRequest request, HttpServletResponse response) throws IOException {
+	boolean displayDocs1(@RequestParam(value = "username", required = true) String username,
+			@RequestParam(value = "password", required = true) String password, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
 		System.out.println("Authentication.............");
 		boolean status = false;
 
@@ -54,7 +51,7 @@ public class DailyReportController {
 			e.printStackTrace();
 		}
 
-		if(status){
+		if (status) {
 			HttpSession session = request.getSession();
 			session.setAttribute("username", username);
 			session.setAttribute("password", password);
@@ -68,42 +65,118 @@ public class DailyReportController {
 	}
 
 	@RequestMapping("/view")
-	ModelAndView view(Model model,HttpServletRequest request, HttpServletResponse response) throws RestClientException, 
-	InterruptedException, ExecutionException, URISyntaxException {
+	ModelAndView view(Model model, HttpServletRequest request, HttpServletResponse response)
+			throws RestClientException, InterruptedException, ExecutionException, URISyntaxException {
 		System.out.println("VIEWING....................");
 		System.out.println();
 
-		String username = (String)request.getSession().getAttribute("username");
+		String username = (String) request.getSession().getAttribute("username");
+		String password = (String) request.getSession().getAttribute("password");
 
-		if(username == null){
+		if (username == null) {
 			return new ModelAndView("index");
 		}
 
-
 		ModelAndView view = new ModelAndView("view");
 		view.addObject("username", request.getSession().getAttribute("username"));
-		view.addObject("table", getJiraTickets());
+		view.addObject("table", getJiraTickets(username, password));
 		return view;
 
 	}
 
-	private Iterable<Issue> getJiraTickets() throws URISyntaxException, InterruptedException, ExecutionException {
+	private List<JiraDTO> getJiraTickets(String username, String password)
+			throws URISyntaxException, InterruptedException, ExecutionException {
 		JiraRestClientFactory restClientFactory = new AsynchronousJiraRestClientFactory();
 		final URI jiraServerUri = new URI("http://jira.lexisnexis.fr/");
-		JiraRestClient restClient = restClientFactory.createWithBasicHttpAuthentication(jiraServerUri, "ummerfas", "Paris@222");
+		JiraRestClient restClient = restClientFactory.createWithBasicHttpAuthentication(jiraServerUri, username,
+				password);
 		System.out.println(restClient.getSessionClient().getCurrentSession().get().getUsername());
 
-		Promise<SearchResult> results = restClient.getSearchClient().searchJql("assignee in (UMMERFAS, RIZWANS, BASKARS2) AND Sprint in openSprints() ORDER BY assignee ASC, status ASC");
+		Promise<SearchResult> results = restClient.getSearchClient().searchJql(
+				"assignee in (UMMERFAS, RIZWANS, BASKARS2) AND Sprint in openSprints() ORDER BY assignee ASC, status ASC");
 
-		for(BasicIssue issue : results.get().getIssues()){
+		List<JiraDTO> ticketList = new ArrayList<JiraDTO>();
+
+		for (BasicIssue issue : results.get().getIssues()) {
+			JiraDTO jira = new JiraDTO();
 			String key = issue.getKey();
 			Issue ticket = restClient.getIssueClient().getIssue(key).get();
 
-			if(ticket.getKey().equalsIgnoreCase("GC-1099"))
-				System.out.println(ticket);
+			jira.setTicketNumber(ticket.getKey());
+			
+			jira.setAssignee(validate(ticket, "assignee"));
+			jira.setEstimated(validate(ticket, "estimated"));
+			jira.setLogged(validate(ticket, "logged"));
+			jira.setRemaining(validate(ticket, "remaining"));
+			jira.setReporter(validate(ticket,"reporter"));
+			jira.setResolution(validate(ticket,"resoultion"));
+			jira.setStatus(validate(ticket, "status"));
 
+			jira.setComponents(nullCheckString(ticket.getComponents().toString()));
+			jira.setCreated(nullCheckString(ticket.getCreationDate().toString()));
+			jira.setDescription(nullCheckString(ticket.getDescription()));
+			jira.setLabels(nullCheckString(ticket.getLabels().toString()));
+			jira.setPriority(nullCheckString(ticket.getPriority().getName()));
+			jira.setType(nullCheckString(ticket.getIssueType().getName()));
+			jira.setUpdated(nullCheckString(ticket.getUpdateDate().toString()));
+
+			ticketList.add(jira);
 		}
-		
-		return results.get().getIssues();
+
+		return ticketList;
 	}
+
+	private String validate(Issue ticket, String tobeChecked) {
+
+		switch(tobeChecked){
+		case "estimated":
+			return (nullCheckObject(ticket.getTimeTracking()) && ticket.getTimeTracking().getOriginalEstimateMinutes() != null)
+					? ticket.getTimeTracking().getOriginalEstimateMinutes().toString() : "";
+
+		case "logged":
+			return (nullCheckObject(ticket.getTimeTracking()) && ticket.getTimeTracking().getTimeSpentMinutes() != null)
+					? ticket.getTimeTracking().getTimeSpentMinutes().toString() : "";
+
+		case "remaining":
+			return (nullCheckObject(ticket.getTimeTracking()) &&  ticket.getTimeTracking().getRemainingEstimateMinutes() != null)
+					? ticket.getTimeTracking().getRemainingEstimateMinutes().toString() : "";
+					
+		case "assignee":
+			return (nullCheckObject(ticket.getAssignee()) && nullCheckObject(ticket.getAssignee().getDisplayName()))
+					? ticket.getAssignee().getDisplayName() : "";
+					
+		case "reporter":
+			return (nullCheckObject(ticket.getReporter()) && ticket.getReporter().getDisplayName() != null) 
+					? ticket.getReporter().getDisplayName() : "";
+					
+		case "resolution":
+			return  (nullCheckObject(ticket.getResolution()) && ticket.getResolution().getName() != null) 
+					? ticket.getResolution().getName() : "";
+					
+		case "status":
+			return (nullCheckObject(ticket.getStatus()) && ticket.getStatus().getName() != null) 
+					? ticket.getStatus().getName() : "";
+
+		default:
+			return "";
+		}
+	}
+
+	private boolean nullCheckObject(Object tobeChecked) {
+		if (tobeChecked != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	private String nullCheckString(String tobeChecked) {
+		if (tobeChecked != null) {
+			return tobeChecked;
+		} else {
+			return "";
+		}
+	}
+
 }
--- src/main/java/com/suf/wsr/daily_report/controller/JiraDTO.java
+++ src/main/java/com/suf/wsr/daily_report/controller/JiraDTO.java
@@ -2,6 +2,122 @@ package com.suf.wsr.daily_report.controller;
 
 public class JiraDTO {
 	
+	private String ticketNumber;
+	private String status;
+	private String resolution;
+	private String assignee;
+	private String reporter;
+	private String description;
+	private String created;
+	private String updated;
+	private String estimated;
+	private String remaining;
+	private String logged;
+	private String sprint;
+	private String components;
+	private String labels;
+	private String priority;
+	private String type;
+	
+	public String getTicketNumber() {
+		return ticketNumber;
+	}
+	public void setTicketNumber(String ticketNumber) {
+		this.ticketNumber = ticketNumber;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getResolution() {
+		return resolution;
+	}
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+	public String getAssignee() {
+		return assignee;
+	}
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+	public String getReporter() {
+		return reporter;
+	}
+	public void setReporter(String reporter) {
+		this.reporter = reporter;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getUpdated() {
+		return updated;
+	}
+	public void setUpdated(String updated) {
+		this.updated = updated;
+	}
+	public String getEstimated() {
+		return estimated;
+	}
+	public void setEstimated(String estimated) {
+		this.estimated = estimated;
+	}
+	public String getRemaining() {
+		return remaining;
+	}
+	public void setRemaining(String remaining) {
+		this.remaining = remaining;
+	}
+	public String getLogged() {
+		return logged;
+	}
+	public void setLogged(String logged) {
+		this.logged = logged;
+	}
+	public String getSprint() {
+		return sprint;
+	}
+	public void setSprint(String sprint) {
+		this.sprint = sprint;
+	}
+	public String getComponents() {
+		return components;
+	}
+	public void setComponents(String components) {
+		this.components = components;
+	}
+	public String getLabels() {
+		return labels;
+	}
+	public void setLabels(String labels) {
+		this.labels = labels;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+ 
+	
+	
 	
 
 }
